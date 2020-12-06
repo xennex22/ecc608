4c238fd098251171d6c8c43dfd98af2be836d72c
xennex22 <25083624+xennex22@users.noreply.github.com>
2020-12-06 11:48:11 -0800

    Add files via upload

---
+++ ecc608_verify.h
@@ -0,0 +1,14 @@
+#ifndef _ECC608_VERIFY_H_
+#define _ECC608_VERIFY_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern bool verify_device_cert(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // _ECC608_VERIFY_H_
